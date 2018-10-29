19c5a191e6dfd9727fb54c9aec5db9491552a585
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-29 09:54:54 +0100

    Example #7: matrix subsets

---
+++ incanter-2/07_matrix_subset.clj
@@ -0,0 +1,42 @@
+; ---------------------------------------------------
+; příklad číslo 7 - přístup k podmaticím
+; ---------------------------------------------------
+
+; matice 5x5 prvků
+(def M5 (matrix (range 25) 5))
+
+; výběr jednoho prvku (prostředního) z matice
+(println (sel M5 2 2))
+
+; výběr sloupců
+(println (sel M5 :cols [0]))
+(println (sel M5 :cols [1 2]))
+(println (sel M5 :cols [2 1]))
+(println (sel M5 :cols (range 1 4)))
+
+; otočení sloupců
+(println (sel M5 :cols (range 4 -1 -1)))
+
+; výběr řádků
+(println (sel M5 :rows [0]))
+(println (sel M5 :rows [1 2]))
+(println (sel M5 :rows [2 1]))
+(println (sel M5 :rows (range 1 4)))
+
+; otočení řádků
+(println (sel M5 :rows (range 4 -1 -1)))
+
+; bez vybraných sloupců
+(println (sel M5 :except-cols [0]))
+(println (sel M5 :except-cols [1 2]))
+(println (sel M5 :except-cols (range 1 4)))
+
+; bez vybraných řádků
+(println (sel M5 :except-rows [0]))
+(println (sel M5 :except-rows [1 2]))
+(println (sel M5 :except-rows (range 1 4)))
+
+; rozdíly mezi selektory :cols :rows a :all
+(println (sel M5 :rows [1 3 4]))
+(println (sel M5 :cols [1 3 4]))
+(println (sel M5 :all [1 3 4]))
