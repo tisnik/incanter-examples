1d552f796719f9d55ca8c3593e0527bf577ce22c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-14 09:37:34 +0100

    Example #1: parametric plot

---
+++ incanter-4/01_parametric_plot_A.clj
@@ -0,0 +1,26 @@
+(ns parametric-graph
+  (:use (incanter core stats charts svg)))
+
+
+(defn spiral
+    [t]
+    [(* t (cos t)) (* t (sin t))])
+
+(view (parametric-plot spiral 0 (* 6 Math/PI) :x-label "x" :y-label "y"))
+
+(defn lis
+    [t] 
+    [(cos (* t 3)) (sin (* t 4))])
+
+(view (parametric-plot lis 0 (* 6 Math/PI) :x-label "x" :y-label "y"))
+
+(defn spiral-2
+    [t]
+    [(+ (* t (cos t))
+        (* 0.4 (sin (* 20 t))))
+     (+ (* t (sin t))
+        (* 0.4 (cos (* 20 t))))])
+
+(-> (parametric-plot spiral 0 (* 6 Math/PI) :x-label "x" :y-label "y")
+    (add-parametric spiral-2 0 (* 6 Math/PI))
+    view)
