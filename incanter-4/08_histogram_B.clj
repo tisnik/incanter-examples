f892c9c876e37906c5bdc5b6a87ca3f8ba7426d5
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-14 09:43:42 +0100

    Example #8: custom histogram

---
+++ incanter-4/08_histogram_B.clj
@@ -0,0 +1,20 @@
+(ns histogram-B
+  (:use (incanter core stats charts svg)))
+
+(def hody-kostkou   (take 5000 (repeatedly #(inc (rand-int 6)))))
+(def hody-kostkou-2 (take 5000 (repeatedly #(inc (rand-int 6)))))
+(def hody-kostkou-3 (take 5000 (repeatedly #(inc (rand-int 6)))))
+
+(defn bar-chart-histogram
+    [values]
+    (let [freq (frequencies values)
+          f    #(freq % 0)
+          ks   (keys freq)
+          a    (apply min ks)
+          b    (apply max ks)
+          x-values (range a (inc b))
+          x-labels (map str x-values)
+          y-values (map f x-values)]
+      (bar-chart x-labels y-values)))
+
+(view (bar-chart-histogram (map #(+ %1 %2 %3) hody-kostkou hody-kostkou-2 hody-kostkou-3)))
